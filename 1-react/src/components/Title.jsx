dc5fb148bc414b86bfeb69f30d32689bdf24a5e1
parkhojeong <parkhj062@gmail.com>
2024-10-27 23:34:56 +0900

    [1.4장 장바구니 화면] 1.4.3 Title 다시보기(실습)

--- 1-react/src/components/Title.jsx
+++ 1-react/src/components/Title.jsx
@@ -1,3 +1,11 @@
-const Title = ({ children }) => <h1>{children}</h1>;
+const Title = ({ hasBackBtn = false, children }) => {
+  return (
+    <>
+      {hasBackBtn && <button>Back</button>}
+      <h1>{children}</h1>
+    </>
+  )
+
+}
 
 export default Title;
