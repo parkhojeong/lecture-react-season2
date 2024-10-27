5f4dc9cc221a2f7d94db19244dd32bddf38f7bb1
parkhojeong <parkhj062@gmail.com>
2024-10-27 18:47:08 +0900

    [1.3장 주문 내역 화면] 1.3.3 Card

--- 1-react/src/pages/OrderPage/index.jsx
+++ 1-react/src/pages/OrderPage/index.jsx
@@ -1,11 +1,12 @@
 import Page from "../../components/Page";
 import Title from "../../components/Title";
 import Navbar from "../../components/Navbar";
+import Card from "../../components/Card";
 
 const OrderPage = () => (
   <div className="OrderPage">
     <Page header={<Title>주문내역</Title>} footer={<Navbar />}>
-      여기에 메인 컨텐츠를 담는다.
+      <Card />
     </Page>
   </div>
 );
