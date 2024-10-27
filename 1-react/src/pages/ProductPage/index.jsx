e256e0d764cba62676f28f26bdb61dba2f35f6a4
parkhojeong <parkhj062@gmail.com>
2024-10-27 18:31:28 +0900

    [1.2장 상품목록 화면] 1.2.11 상태정의

--- 1-react/src/pages/ProductPage/index.jsx
+++ 1-react/src/pages/ProductPage/index.jsx
@@ -1,7 +1,10 @@
+import React from 'react';
+import ProductApi from "shared/api/ProductApi";
 import Page from "../../components/Page";
 import Title from "../../components/Title";
 import Navbar from "../../components/Navbar";
 import ProductItem from "../../components/ProductItem";
+
 
 const fakeProduct = {
   id: "CACDA421",
@@ -10,18 +13,42 @@ const fakeProduct = {
   thumbnail: "./images/menu-해물계란라면.jpg",
 };
 
-const ProductPage = () => {
-  return (
+class ProductPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      productList: []
+    }
+  }
+
+  componentDidMount() {
+    this.fetch();
+  }
+
+  async fetch() {
+    try{
+      const productList = await ProductApi.fetchProductList();
+      this.setState({productList});
+    }catch (e){
+      console.error(e);
+    }
+  }
+
+  render(){
+    return (
     <div className="ProductPage">
       <Page header={<Title>메뉴목록</Title>} footer={<Navbar/>}>
         <ul>
-          <li>
-            <ProductItem product={fakeProduct}/>
-          </li>
-        </ul>
-      </Page>
+          {this.state.productList.map(product => (
+            <li key={product.id}>
+            <ProductItem product={product}/>
+            </li>
+        ))}
+
+      </ul>
+    </Page>
     </div>
-  )
+  )}
 }
 
 export default ProductPage;
