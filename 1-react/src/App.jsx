e8000b9370a4dc08e1b480f7f331861132931bf7
parkhojeong <parkhj062@gmail.com>
2024-10-27 18:22:57 +0900

    [1.2장 상품목록 화면] 1.2.10 ProductPage

--- 1-react/src/App.jsx
+++ 1-react/src/App.jsx
@@ -1,25 +1,7 @@
-import Navbar from "./components/Navbar";
-import ProductItem from "./components/ProductItem";
-import Title from "./components/Title";
-import Page from "./components/Page";
-
-const fakeProduct = {
-  id: "CACDA421",
-  name: "해물 계란 라면",
-  price: "6000",
-  thumbnail: "./images/menu-해물계란라면.jpg",
-};
+import ProductPage from "./pages/ProductPage";
 
 const App = () => (
-  <div className="ProductPage">
-    <Page header={<Title>메뉴목록</Title>} footer={<Navbar />}>
-      <ul>
-        <li>
-          <ProductItem product={fakeProduct} />
-        </li>
-      </ul>
-    </Page>
-  </div>
+  <ProductPage />
 );
 
 export default App;
