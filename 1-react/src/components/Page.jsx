38a23309fe6d352aab3d832a4cfe2c15a3bf6d9c
parkhojeong <parkhj062@gmail.com>
2024-10-27 18:19:07 +0900

    [1.2장 상품목록 화면] 1.2.9 Page

---
+++ 1-react/src/components/Page.jsx
@@ -0,0 +1,12 @@
+const Page = ({header, footer, children}) => {
+
+  return (
+    <div className="Page">
+      <header>{header}</header>
+      <main>{children}</main>
+      <footer>{footer}</footer>
+    </div>
+  )
+}
+
+export default Page;
