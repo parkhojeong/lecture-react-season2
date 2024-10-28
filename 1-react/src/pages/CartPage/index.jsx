764bdb950b116aeb48b674e7d94c3277abd53a14
parkhojeong <parkhj062@gmail.com>
2024-10-28 23:21:33 +0900

    [1.4장 장바구니 화면] 1.4.14 상태 정의(실습)

--- 1-react/src/pages/CartPage/index.jsx
+++ 1-react/src/pages/CartPage/index.jsx
@@ -1,8 +1,10 @@
+import React from "react";
 import Page from "../../components/Page";
 import ProductItem from "../../components/ProductItem";
 import Title from "../../components/Title";
 import OrderForm from "./OrderForm";
 import PaymentButton from "./PaymentButton";
+import ProductApi from "shared/api/ProductApi";
 
 const fakeProduct = {
   id: "CACDA420",
@@ -11,16 +13,39 @@ const fakeProduct = {
   thumbnail: "./images/menu-해물계란라면.jpg",
 };
 
-const CartPage = () => (
-  <div className="CartPage">
-    <Page
-      header={<Title backUrl="/">장바구니</Title>}
-      footer={<PaymentButton />}
-    >
-      <ProductItem product={fakeProduct} />
-      <OrderForm />
-    </Page>
-  </div>
-);
+class CartPage extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      product: null
+    }
+  }
+
+
+  async fetch(id) {
+    const product = await ProductApi.fetchProduct(id);
+    this.setState({product})
+  }
+
+  componentDidMount() {
+    this.fetch("CACDA420");
+  }
+
+  render(){
+    const {product} = this.state;
+    return (
+      <div className="CartPage">
+        <Page
+          header={<Title backUrl="/">장바구니</Title>}
+          footer={<PaymentButton />}
+        >
+          {product && <ProductItem product={product} />}
+          <OrderForm />
+        </Page>
+      </div>
+    );
+  }
+}
 
 export default CartPage;
