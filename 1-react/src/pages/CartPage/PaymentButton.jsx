bf48410be9cb74fed6a9959f4718f86d7e1365bc
parkhojeong <parkhj062@gmail.com>
2024-10-28 23:10:39 +0900

    [1.4장 장바구니 화면] 1.4.12 PaymentButton(실습)

---
+++ 1-react/src/pages/CartPage/PaymentButton.jsx
@@ -0,0 +1,10 @@
+import Button from "../../components/Button";
+
+const PaymentButton = () => {
+
+  return (
+    <Button styleType="brand-solid" block form="order-form">
+      결제하기
+    </Button>
+  )
+}
