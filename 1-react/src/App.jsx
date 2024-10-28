d4df01901d6c9b65b88df82068c1272b7780bc49
parkhojeong <parkhj062@gmail.com>
2024-10-28 22:29:56 +0900

    [1.4장 장바구니 화면] 1.4.9 Ref와 Dom
    - ref를 이용하여 절차적 코드 작성 가능. element에 직접 접근 혹은 컴포넌트에 직접 접근 가능

--- 1-react/src/App.jsx
+++ 1-react/src/App.jsx
@@ -10,4 +10,39 @@ const App = () => (
   </>
 );
 
-export default App;
+// export default App;
+
+import React from "react";
+
+class Foo extends React.Component {
+  render(){
+    return <>Foo</>
+  }
+}
+
+class MyComponent extends React.Component {
+  divRef = React.createRef();
+  fooRef = React.createRef();
+
+
+  constructor(props) {
+    super(props);
+    console.log("constructor", this.divRef)
+  }
+
+  render() {
+    console.log("render", this.divRef)
+    return <>
+      <div ref={this.divRef} />
+      <Foo ref={this.fooRef} />
+    </>
+  }
+
+  componentDidMount() {
+    console.log("componentDidMount divRef", this.divRef)
+    console.log("componentDidMount fooRef", this.fooRef)
+  }
+
+}
+
+export default MyComponent
