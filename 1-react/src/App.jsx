c823f52435198f529beb075dea947fc2efb85a8b
parkhojeong <parkhj062@gmail.com>
2024-10-29 22:21:51 +0900

    [2.1장 컨택스트] 2.1.3, 2.1.4
    - 인접한 컴포넌트 간 인자를 전달하는 구조적인 한계 해결
    - 멀리 떨어진 구조에서도 Provider 하위에 Consumer를 사용하기만 하면 Provider의 메시지를 구독하는 구조

--- 1-react/src/App.jsx
+++ 1-react/src/App.jsx
@@ -10,4 +10,57 @@ const App = () => (
   </>
 );
 
-export default App;
+// export default App;
+
+
+import MyReact from "./lib/MyReact";
+import React from "react";
+
+const countContext = MyReact.createContext({
+  count: 0,
+  setCount: () => {},
+})
+
+class CountProvider extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0
+    }
+  }
+
+  render() {
+    const value = {
+      count: this.state.count,
+      setCount: nextValue => this.setState({count: nextValue})
+    }
+
+      return (<countContext.Provider value={value}>
+        {this.props.children}
+      </countContext.Provider>
+      )
+  }
+}
+
+const Count = () => {
+  return (<countContext.Consumer>
+    {(value) => <div>{value.count}</div>}
+  </countContext.Consumer>
+  )
+}
+
+const PlusButton = () => {
+  return (<countContext.Consumer>
+      {(value) => (
+        <button onClick={() => value.setCount(value.count + 1)}>+ 카운트 올리기</button>
+        )}
+    </countContext.Consumer>
+  )
+}
+
+export default () => (
+  <CountProvider>
+    <Count />
+    <PlusButton />
+  </CountProvider>
+)
