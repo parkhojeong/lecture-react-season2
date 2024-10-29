76be2e3597945311f429f702a2f45e74bcf897b6
parkhojeong <parkhj062@gmail.com>
2024-10-29 23:24:29 +0900

    [2.2장 라우터 1] 2.2.7 Route(풀이)
    - 조건부 렌더링 등의 절차적인 방식이 아닌 선언적인 방식으로 라우팅 테이블 만듦

--- 1-react/src/App.jsx
+++ 1-react/src/App.jsx
@@ -1,8 +1,15 @@
 import * as MyRouter from "./lib/MyRouter";
+import CartPage from "1-react/src/pages/CartPage";
+import ProductPage from "1-react/src/pages/ProductPage";
+import OrderPage from "1-react/src/pages/OrderPage";
 
 const App = () => (
   <MyRouter.Router>
-    <MyRouter.Routes />
+    <MyRouter.Routes >
+      <MyRouter.Route path={"/cart"} element={<CartPage />}/>
+      <MyRouter.Route path={"/order"} element={<OrderPage />}/>
+      <MyRouter.Route path={"/"} element={<ProductPage />}/>
+    </MyRouter.Routes>
   </MyRouter.Router>
 );
 
--- 1-react/src/lib/MyRouter.jsx
+++ 1-react/src/lib/MyRouter.jsx
@@ -33,21 +33,33 @@ export class Router extends React.Component {
   }
 }
 
-export const Routes = () => (
-      <>
-        <Route path={"/cart"} element={<CartPage />}/>
-        <Route path={"/order"} element={<OrderPage />}/>
-        <Route path={"/"} element={<ProductPage />}/>
-      </>
+export const Route = () => null;
+
+export const Routes = ({children}) => (
+      <routerContext.Consumer>
+        {({path}) => {
+          let selectedRoute = null;
+          React.Children.forEach(children, (child) => {
+            // 리액트 엘리먼트인지 검사
+            if(!React.isValidElement(child)) return;
+
+            // 프래그먼트 검사
+            if(child.type === React.Fragment) return;
+
+            // Route 컴포넌트인지 검사
+            if(!child.props.path || !child.props.element) return;
+
+            // 요청 경로를 검사
+            if(child.props.path !== path.replace(/\?.*$/, "")) return;
+
+            selectedRoute = child.props.element;
+          })
+
+          return selectedRoute;
+        }}
+      </routerContext.Consumer>
 );
 
-export const Route = ({path: to, element}) => (
-  <routerContext.Consumer>
-    {({ path }) => (
-        to === path && element
-    )}
-  </routerContext.Consumer>
-);
 
 export const Link = ({ to, ...rest }) => (
   <routerContext.Consumer>
