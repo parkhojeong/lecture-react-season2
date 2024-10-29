c431de2225f646359b3cbdaf2dd7ac8542114111
parkhojeong <parkhj062@gmail.com>
2024-10-29 19:15:47 +0900

    [2.1장 컨택스트] 2.1.2 이벤트 에미터

--- 1-react/src/App.jsx
+++ 1-react/src/App.jsx
@@ -1,6 +1,7 @@
 import CartPage from "./pages/CartPage";
 import OrderPage from "./pages/OrderPage";
 import ProductPage from "./pages/ProductPage";
+import createEventEmitter from "shared/lib/EventEmitter";
 
 const App = () => (
   <>
@@ -11,3 +12,13 @@ const App = () => (
 );
 
 export default App;
+
+const eventEmitter = createEventEmitter(0);
+const logger = value => console.log(value);
+
+eventEmitter.on(logger)
+console.log(eventEmitter.get()); //0
+eventEmitter.set(1) //1
+eventEmitter.set(2) //2
+
+setTimeout(() => eventEmitter.set(10), 3000);
---
+++ shared/lib/EventEmitter.js
@@ -0,0 +1,22 @@
+const createEventEmitter = (value) => {
+  let handlers = [];
+
+  const on = handler => handlers.push(handler);
+  const off = handler => {
+    handlers = handlers.filter(h=> h!== handler)
+  }
+  const get = () => value;
+  const set = newValue => {
+    value = newValue;
+    handlers.forEach(handler => handler(value))
+  }
+
+  return {
+    on,
+    off,
+    get,
+    set
+  }
+}
+
+export default createEventEmitter;
