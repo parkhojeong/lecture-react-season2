1d2eb35a20d920a4c4a6da7cd8aee947d2501a8c
parkhojeong <parkhj062@gmail.com>
2024-10-29 22:36:10 +0900

    [2.2장 라우터 1] 2.2.2 경로에 따라 컴포넌트 렌더링
    - webpack server에서 url 요청에 대한 리소스 없을 경우 fallback으로 index.html 리턴하도록 설정
    - todo: 브라우저에서 라우팅하도록 변경

--- 1-react/src/App.jsx
+++ 1-react/src/App.jsx
@@ -2,12 +2,14 @@ import CartPage from "./pages/CartPage";
 import OrderPage from "./pages/OrderPage";
 import ProductPage from "./pages/ProductPage";
 
-const App = () => (
-  <>
-    {/* <ProductPage /> */}
-    {/* <OrderPage /> */}
-    <CartPage />
+const App = () => {
+  const {pathname} = window.location;
+
+  return <>
+    {pathname === '/order' && <OrderPage />}
+    {pathname === '/cart' && <CartPage/>}
+    {!['/order', '/cart'].includes(pathname) && <ProductPage />}
   </>
-);
+};
 
 export default App;
