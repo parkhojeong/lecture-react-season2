87812a2fd466cfbf935674c4b78c26b9f5e3d8c2
parkhojeong <parkhj062@gmail.com>
2024-10-31 00:06:21 +0900

    [2.4장 다이얼로그 1] 2.4.1 Dialog

--- 1-react/src/App.jsx
+++ 1-react/src/App.jsx
@@ -2,6 +2,8 @@ import * as MyRouter from "./lib/MyRouter";
 import CartPage from "./pages/CartPage";
 import OrderPage from "./pages/OrderPage";
 import ProductPage from "./pages/ProductPage";
+import Dialog from "./components/Dialog";
+import Backdrop from "./components/Backdrop";
 
 const App = () => (
   <MyRouter.Router>
@@ -10,6 +12,9 @@ const App = () => (
       <MyRouter.Route path="/order" element={<OrderPage />} />
       <MyRouter.Route path="/" element={<ProductPage />} />
     </MyRouter.Routes>
+    <Backdrop>
+      <Dialog />
+    </Backdrop>
   </MyRouter.Router>
 );
 
---
+++ 1-react/src/components/Backdrop.jsx
@@ -0,0 +1,7 @@
+const Backdrop = ({children}) => {
+  return (
+    <div className={"Backdrop"}>{children}</div>
+  )
+}
+
+export default Backdrop
