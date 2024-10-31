501c9923998caddd338046870d065e45c7429b62
parkhojeong <parkhj062@gmail.com>
2024-10-31 23:29:18 +0900

    [2.5장 다이얼로그 2] 2.5.2 리액트 포탈

--- 1-react/src/main.js
+++ 1-react/src/main.js
@@ -8,5 +8,23 @@ worker.start({
 });
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+const root2 = ReactDOM.createRoot(document.getElementById("dialog"));
 
-root.render(<App />);
+// root.render(<App />);
+
+import ReactDOM2 from 'react-dom';
+const Red = () => {
+  return ReactDOM2.createPortal(
+    <div className={"red"}>Red</div>,
+    document.getElementById("dialog")
+    );
+}
+const Green = () => {
+  return <div className={"green"} onClick={(e) => {console.log(e)}}>
+    <div>Green</div>
+    <Red />
+  </div>
+}
+
+root.render(<Green />)
+// root2.render(<Red />)
