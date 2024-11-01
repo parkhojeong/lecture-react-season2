c313943ed9dfbb2cb4e206a7f2070f3805efb360
parkhojeong <parkhj062@gmail.com>
2024-11-01 15:52:09 +0900

    [3.1장 클래스/함수 컴포넌트] 3.1.2 클래스와 함수

--- 2-hook/src/App.jsx
+++ 2-hook/src/App.jsx
@@ -1,3 +1,28 @@
 const App = () => <>2-hook</>;
 
 export default App;
+
+class Contract {
+  constructor(name) {
+    this.name = name;
+  }
+
+  sign() {
+    const capturedName = this.name;
+    setTimeout(() => console.log('서명인: ', capturedName) , 3000)
+  }
+}
+
+// const contract = new Contract('사용자 1');
+// contract.sign();
+// contract.name = '사용자 2'
+
+function createContract(name) {
+  const sign = () => {
+    setTimeout(() => console.log('서명인: ', name) , 3000)
+  }
+
+  return {sign}
+}
+const contract = createContract('사용자 3')
+contract.sign()
