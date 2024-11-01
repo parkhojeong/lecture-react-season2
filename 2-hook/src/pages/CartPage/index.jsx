f470c16360b9fba206758625707a4e6a01f73273
parkhojeong <parkhj062@gmail.com>
2024-11-01 23:46:36 +0900

    [3.4장 훅 활용 1] 3.4.2 장바구니 화면(실습)

--- 2-hook/src/pages/CartPage/index.jsx
+++ 2-hook/src/pages/CartPage/index.jsx
@@ -1,3 +1,41 @@
-const CartPage = () => <>CartPage</>;
+import Page from "../../components/Page";
+import Title from "../../components/Title";
+import PaymentButton from "../../pages/CartPage/PaymentButton";
+import ProductItem from "../../components/ProductItem";
+// import OrderForm from "../../pages/CartPage/OrderForm";
+import React from "react";
+import ProductApi from "shared/api/ProductApi";
+import ErrorDialog from "../../components/ErrorDialog";
+
+const CartPage = () => {
+  const [product, setProduct] = React.useState(null);
+
+   const fetch = async () => {
+     try {
+       const product = await ProductApi.fetchProduct("CACDA421");
+       setProduct(product);
+     } catch (e) {
+       // openDialog(<ErrorDialog />);
+       return;
+     }
+  }
+
+  React.useEffect(() => {
+    fetch()
+  }, [])
+
+
+  return (
+    <div className="CartPage">
+      <Page
+        header={<Title backUrl="/">장바구니</Title>}
+        footer={<PaymentButton />}
+      >
+        {product && <ProductItem product={product} />}
+        {/*<OrderForm onSubmit={this.handleSubmit} />*/}
+      </Page>
+    </div>
+  );
+};
 
 export default CartPage;
