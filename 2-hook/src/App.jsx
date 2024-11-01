f6ac6debc0e2dc0c54ee88e292c4e08f62c3ea16
parkhojeong <parkhj062@gmail.com>
2024-11-01 16:21:34 +0900

    [3.1장 클래스/함수 컴포넌트] 3.1.3 리액트 컴포넌트

--- 2-hook/src/App.jsx
+++ 2-hook/src/App.jsx
@@ -1,28 +1,40 @@
-const App = () => <>2-hook</>;
+import React from "react";
+
+// const App = () => <>2-hook</>;
+
+// export default App;
+
+class Contract extends React.Component {
+  render() {
+    const props = this.props;
+    const sign = () => {
+      setTimeout(() => console.log('서명인: ', props, this.props) , 3000);
+    }
+    return <button onClick={sign}>sign</button>
+  }
+}
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '사용자1'
+    }
+  }
+
+  handleChange(e) {
+    this.setState({name: e.target.value})
+  }
+
+  render() {
+    return <>
+      <select value={this.state.name} onChange={this.handleChange.bind(this)}>
+        <option value={"사용자1"}>사용자1</option>
+        <option value={"사용자2"}>사용자2</option>
+      </select>
+      <Contract name={this.state.name}/>
+    </>
+  }
+}
 
 export default App;
-
-class Contract {
-  constructor(name) {
-    this.name = name;
-  }
-
-  sign() {
-    const capturedName = this.name;
-    setTimeout(() => console.log('서명인: ', capturedName) , 3000)
-  }
-}
-
-// const contract = new Contract('사용자 1');
-// contract.sign();
-// contract.name = '사용자 2'
-
-function createContract(name) {
-  const sign = () => {
-    setTimeout(() => console.log('서명인: ', name) , 3000)
-  }
-
-  return {sign}
-}
-const contract = createContract('사용자 3')
-contract.sign()
