c0cec86ebf27ff11c028a3e939fdaf716d908718
parkhojeong <parkhj062@gmail.com>
2024-11-01 23:58:37 +0900

    [3.4장 훅 활용 1] 3.4.4 주문 내역 화면(실습)

--- 2-hook/src/pages/OrderPage/index.jsx
+++ 2-hook/src/pages/OrderPage/index.jsx
@@ -1,3 +1,44 @@
-const OrderPage = () => <>OrderPage</>;
+import Page from "../../components/Page";
+import Title from "../../components/Title";
+import Navbar from "../../components/Navbar";
+import OrderStatusCard from "../../pages/OrderPage/OrderStatusCard";
+import OrderPaymentCard from "../../pages/OrderPage/OrderPaymentCard";
+import OrderDeliveryCard from "../../pages/OrderPage/OrderDeliveryCard";
+import React from "react";
+import OrderApi from "shared/api/OrderApi";
+// import ErrorDialog from "../../components/ErrorDialog";
+
+
+const OrderPage = () => {
+  const [order, setOrder] = React.useState(null);
+
+  const fetch = async () => {
+    try {
+      const order = await OrderApi.fetchMyOrder();
+      setOrder( order );
+    } catch (e) {
+      // openDialog(<ErrorDialog />);
+      return;
+    }
+  }
+
+  React.useEffect(() => {
+    fetch();
+  }, [])
+
+  return (
+    <div className="OrderPage">
+      <Page header={<Title>주문내역</Title>} footer={<Navbar />}>
+        {order && (
+          <>
+            <OrderStatusCard order={order} />
+            <OrderPaymentCard order={order} />
+            <OrderDeliveryCard order={order} />
+          </>
+        )}
+      </Page>
+    </div>
+  );
+};
 
 export default OrderPage;
