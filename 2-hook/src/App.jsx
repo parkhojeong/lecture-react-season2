47a48f04b1f24b3574ecccf3fa276b72786df92a
parkhojeong <parkhj062@gmail.com>
2024-11-13 12:47:46 +0900

    [3.5장 컨택스트 훅] 3.5.2 useContenxt

--- 2-hook/src/App.jsx
+++ 2-hook/src/App.jsx
@@ -1,6 +1,8 @@
 import ProductPage from "./pages/ProductPage";
 import CartPage from "./pages/CartPage";
 import OrderPage from "./pages/OrderPage";
+import MyReact from "2-hook/src/lib/MyReact";
+import React from "react";
 
 const App = () => (
   <>
@@ -10,4 +12,30 @@ const App = () => (
   </>
 );
 
-export default App;
+// export default App;
+
+
+const countContext = MyReact.createContext({})
+
+const CountProvider = ({children}) => {
+  const [count, setCount] = React.useState(0)
+  const value = {count, setCount};
+
+  return <countContext.Provider value={value}>{children}</countContext.Provider>
+}
+
+const Count = () => {
+  const {count} = MyReact.useContext(countContext)
+  return <div>{count}</div>
+}
+
+const PlusButton = () => {
+  const {count, setCount} = MyReact.useContext(countContext);
+  const handleClick = () => setCount(count + 1);
+  return <button onClick={handleClick}>카운트 증가</button>
+}
+
+export default () => <CountProvider>
+  <Count />
+  <PlusButton />
+</CountProvider>
